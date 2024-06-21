35add3b05eac4b4bd16fbe8fb4e6b3aa6a4694c8
Renuka Sharma <renuka.sharma@cloudnation.nl>
2024-06-21 08:43:07 +0200

    fix: fixed conditionally include ip_configuration_id for dynamic nat rules only (#24)

--- modules/nat-rules/main.tf
+++ modules/nat-rules/main.tf
@@ -5,8 +5,11 @@ resource "azurerm_virtual_network_gateway_nat_rule" "rules" {
   mode                       = each.value.mode
   type                       = each.value.type
   virtual_network_gateway_id = try(each.value.virtual_network_gateway_id, var.virtual_network_gateway_id)
-  ip_configuration_id        = each.value.ip_configuration_id
-  resource_group_name        = var.resourcegroup
+
+  # Conditionally assign ip_configuration_id only if the type is "Dynamic"
+  ip_configuration_id = each.value.type == "Dynamic" ? each.value.ip_configuration_id : null
+
+  resource_group_name = var.resourcegroup
 
   external_mapping {
     address_space = each.value.external_mappings.address_space
